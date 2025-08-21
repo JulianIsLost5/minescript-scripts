daa41778e25041cc2f379c2ecd691aa6ab72b7ef
JulianIsLost5 <t.jkunert@web.de>
2025-08-21 21:52:08 +0200

    Create reflection-utils.py

---
+++ reflection-utils.py
@@ -0,0 +1,39 @@
+Array = JavaClass("java.lang.reflect.Array")
+Clazz = JavaClass("java.lang.Class")
+Obj = JavaClass("java.lang.Object")
+
+def call_private_method(obj, intermediary, *args):
+    cls = obj.getClass()
+    
+    param_types = Array.newInstance(type(Clazz), len(args))
+    params = Array.newInstance(type(Obj), len(args))
+    
+    for i, arg in enumerate(args):
+        Array.set(param_types, i, arg.getClass())
+        Array.set(params, i, arg)
+        
+    method = find_method(cls, intermediary, param_types)
+    if method is None:
+        return 
+    
+    method.setAccessible(True)
+    return method.invoke(obj, params)
+    
+def find_method(cls, intermediary, param_types):
+    while cls is not None:
+        try:
+            method = cls.getDeclaredMethod(intermediary, param_types)
+            return method
+        except:
+            pass
+        
+        for interface in cls.getInterfaces():
+            try:
+                method = interface.getDeclaredMethod(intermediary, param_types)
+                return method
+            except:
+                pass
+            
+        cls = cls.getSuperclass()
+    
+    return None
